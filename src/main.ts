5a97c603a3a21115af39957c8a86041aedc44025
Cassio <cassiojr0108@gmail.com>
2023-12-26 22:28:49 -0300

    Configuration of arcthecture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //define uma lista com as propriedades aceitadas
+      forbidNonWhitelisted: true, // recusar requisições se necessario
+      transform: true, // trasnforma os dados da requisição de acordo com o dto
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
 }
 bootstrap();
